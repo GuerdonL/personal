0b22201473142fad9f924f52cf8168532f0abf95
GuerdonL <guerdon@ligon.org>
2023-04-17 14:13:24 -0400

    reworking cards, adding effects

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,9 +1,10 @@
 // ProjectCard.tsx
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './ProjectCard.css';
-import Modal from 'react-modal';
+// import Modal from 'react-modal';
+// import { CSSTransition } from 'react-transition-group';
 
-Modal.setAppElement('#root'); // Replace '#root' with the actual app element ID in your project
+// Modal.setAppElement('#root'); // Replace '#root' with the actual app element ID in your project
 
 interface ProjectCardProps {
   id: string;
@@ -29,35 +30,29 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   projectWriteup,
   links,
 }) => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const cardRef = useRef<HTMLDivElement | null>(null);
 
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (cardRef.current) {
+      const rect=cardRef.current.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      cardRef.current.style.setProperty("--mouse-x", `${x}px`);
+      cardRef.current.style.setProperty("--mouse-y", `${y}px`);
+    }
   };
 
   return (
-    <div className="card-container" onClick={openModal}>
-      <img className="image-header" src={imageUrl} alt={title} />
-      <div className="short-description">{shortDescription}</div>
-
-      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="lightbox">
-        <h2>{title}</h2>
-        <p><strong>Publication Date:</strong> {publicationDate}</p>
-        <p><strong>Full Description:</strong> {fullDescription}</p>
-        <p><strong>Project Writeup:</strong> {projectWriteup}</p>
-        <p>
-          <strong>Links:</strong>
-          {' '}
-          <a href={links.github} target="_blank" rel="noopener noreferrer">GitHub</a>
-          {' | '}
-          <a href={links.liveDemo} target="_blank" rel="noopener noreferrer">Live Demo</a>
-        </p>
-        <button onClick={closeModal}>Close</button>
-      </Modal>
+    <div 
+    ref={cardRef}
+    className="card-container" 
+    onMouseMove={handleMouseMove}
+    >
+      <div className="card-border"></div>
+      <div className="card-content">
+        {/* <img className="image-header" src={imageUrl} alt={title} /> */}
+        <div className="short-description">{shortDescription}</div>
+      </div>
     </div>
   );
 };
--- src/components/Projects/ProjectCardsSection.tsx
+++ src/components/Projects/ProjectCardsSection.tsx
@@ -8,7 +8,7 @@ const projects = [
   {
     id: '1',
     title: 'Weather App',
-    imageUrl: 'https://example.com/weather-app-image.jpg',
+    imageUrl: 'https://www.mayoclinic.org/-/media/kcms/gbs/patient-consumer/images/2013/08/26/10/14/ds00427_im01275_r7_nesbitthu_jpg.jpg',
     shortDescription: 'A simple weather app that displays the current weather and forecast for a given location.',
     fullDescription: 'This weather app was built using React and TypeScript. It uses the OpenWeatherMap API to fetch weather data based on user input. Users can search for a specific location and view the current weather conditions, as well as a 5-day forecast.',
     publicationDate: '2023-05-15',
@@ -18,7 +18,45 @@ const projects = [
       liveDemo: 'https://weather-app.example.com',
     },
   },
-  // Add more projects here
+  {
+    id: '2',
+    title: 'Weather App',
+    imageUrl: 'https://www.mayoclinic.org/-/media/kcms/gbs/patient-consumer/images/2013/08/26/10/14/ds00427_im01275_r7_nesbitthu_jpg.jpg',
+    shortDescription: 'A simple weather app that displays the current weather and forecast for a given location.',
+    fullDescription: 'This weather app was built using React and TypeScript. It uses the OpenWeatherMap API to fetch weather data based on user input. Users can search for a specific location and view the current weather conditions, as well as a 5-day forecast.',
+    publicationDate: '2023-05-15',
+    projectWriteup: 'In this project, I learned how to work with APIs, handle user input, and create a responsive user interface using CSS Grid and Flexbox. I also gained experience with React hooks and TypeScript.',
+    links: {
+      github: 'https://github.com/username/weather-app',
+      liveDemo: 'https://weather-app.example.com',
+    },
+  },
+  {
+    id: '3',
+    title: 'Weather App',
+    imageUrl: 'https://www.mayoclinic.org/-/media/kcms/gbs/patient-consumer/images/2013/08/26/10/14/ds00427_im01275_r7_nesbitthu_jpg.jpg',
+    shortDescription: 'A simple weather app that displays the current weather and forecast for a given location.',
+    fullDescription: 'This weather app was built using React and TypeScript. It uses the OpenWeatherMap API to fetch weather data based on user input. Users can search for a specific location and view the current weather conditions, as well as a 5-day forecast.',
+    publicationDate: '2023-05-15',
+    projectWriteup: 'In this project, I learned how to work with APIs, handle user input, and create a responsive user interface using CSS Grid and Flexbox. I also gained experience with React hooks and TypeScript.',
+    links: {
+      github: 'https://github.com/username/weather-app',
+      liveDemo: 'https://weather-app.example.com',
+    },
+  },
+  {
+    id: '4',
+    title: 'Weather App',
+    imageUrl: 'https://www.mayoclinic.org/-/media/kcms/gbs/patient-consumer/images/2013/08/26/10/14/ds00427_im01275_r7_nesbitthu_jpg.jpg',
+    shortDescription: 'A simple weather app that displays the current weather and forecast for a given location.',
+    fullDescription: 'This weather app was built using React and TypeScript. It uses the OpenWeatherMap API to fetch weather data based on user input. Users can search for a specific location and view the current weather conditions, as well as a 5-day forecast.',
+    publicationDate: '2023-05-15',
+    projectWriteup: 'In this project, I learned how to work with APIs, handle user input, and create a responsive user interface using CSS Grid and Flexbox. I also gained experience with React hooks and TypeScript.',
+    links: {
+      github: 'https://github.com/username/weather-app',
+      liveDemo: 'https://weather-app.example.com',
+    },
+  },
 ];
 
 const ProjectCardsSection = () => {
